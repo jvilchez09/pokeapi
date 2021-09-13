0c74225423ca088e76b545131a070d2bafd65b53
carlos.dominguez.gonzalez.contractor <carlos.dominguez.gonzalez.contractor@bbva.com>
2021-09-14 00:52:52 +0200

    Multiselect part 1

--- src/components/PokeInfo.vue
+++ src/components/PokeInfo.vue
@@ -1,12 +1,11 @@
 <template>
   <v-container>
     <v-row>
-      <!-- <v-card> -->
       <v-container fluid>
         <v-row align="center">
           <v-col cols="6">
             <v-text-field
-              v-model="searchValue"
+              v-model="filterPokemonName"
               placeholder="Search Pokémon"
               id="search-input"
               class="pt-5"
@@ -21,6 +20,8 @@
               chips
               color="primary"
               label="Types"
+              multiple
+              :loading="listPokemonTypes.length==0"
             >
               <template #selection="{ item }">
                 <v-chip close @click:close="close(item)" :color="item">{{
@@ -31,11 +32,6 @@
           </v-col>
         </v-row>
       </v-container>
-      <!-- </v-card> -->
-
-      <!-- <v-card>
-       
-      </v-card> -->
     </v-row>
     <v-row>
       <v-col v-for="(item, i) in filteredPokemons" :key="i" cols="4">
@@ -55,10 +51,7 @@
             {{ item.name }}
           </v-card-title>
           <v-card-subtitle
-            >Type: {{ item.types[0].type.name }}
-            <template v-if="item.types[1]">{{
-              item.types[1].type.name
-            }}</template>
+            >Type:<span v-for="(type, j) in item.types" :key="j"> {{ type.type.name }}</span>
           </v-card-subtitle>
         </v-card>
       </v-col>
@@ -77,12 +70,8 @@ export default {
     pokemons: [],
     sprites: [],
     allInfo: [],
-    type0: [],
-    type1: [],
-    allPokemons: [],
-    searchValue: "",
-    filterPokemonTypes: "",
-    selected: false,
+    filterPokemonName: "",
+    filterPokemonTypes: [],
     colors: {
       fire: "#fda5a570",
       grass: "#a3da8870",
@@ -101,116 +90,53 @@ export default {
       ghost: "#70589870",
       normal: "#bfbfa270",
     },
+    listPokemonTypes: []
   }),
   computed: {
     filteredPokemons() {
-      return this.allInfo.filter(
-        (each) =>
-          each.name.toUpperCase().includes(this.searchValue.toUpperCase()) &&
-          each.types[0].type.name.match(this.filterPokemonTypes)
-        // this.filterPokemonTypes.every(
-        //   (item) => each.types[0].type.name === item
-        // )
-        // this.allPokemon.filter(element => this.listSelect.every(item => element.type === item))
-
-        // this.filterPokemonTypes.every((t) =>
-        //   each.types[0].type.name.includes(t)
-        // )
-      );
-    },
-    listPokemonTypes() {
-      const pokemonTypes = [
-        ...new Set(this.allInfo.map((each) => each.types[0].type.name)),
-      ];
-
-      return pokemonTypes;
+      return this.allInfo.filter(pokemon => pokemon.name.toUpperCase().includes(this.filterPokemonName.toUpperCase()))
     },
   },
-
   async created() {
     for (let i = 0; i < this.pokeList; i++) {
       await PokeInfoServ.fetchAllInfo(i + 1)
         .then((response) => {
+          //console.log(response.data)
           //console.log(response);
           //id con 3 ceros al inicio
           this.id.push(response.data.id.toString().padStart(3, "0")); //.toUpperCase()
           //nombre primera letra mayuscula
           this.pokemons.push(
             response.data.name[0].toUpperCase() + response.data.name.slice(1)
-          ); //.toUpperCase()
+          );
           this.sprites.push(
             response.data.sprites.other["official-artwork"].front_default
           );
           this.allInfo.push(response.data);
-          // console.log(this.pokemons[i].name);
-
-          /**
-           * Agregar los tipos, por ahora solo mostraremos el principal
-           */
-          this.type0.push(response.data.types[0].type.name);
-
-          // if (response.data.types[1].type.name) {
-          //   this.type1.push(response.data.types[1].type.name);
-          // } else {
-          //   this.type1.push(0);
-          // }
         })
         .catch((error) => {
           console.log("hubo un errors" + error);
         });
     }
-    // this.allPokemons = this.allInfo;
-    // console.log(this.allInfo);
+    await this.getPokemonTypes()
   },
   mounted() {
-    // this.names = this.pokemons;
   },
   methods: {
     close(item) {
-      this.selected = !this.selected;
-      // console.log(this.filterPokemonTypes);
-      // console.log(item);
-      // this.filterPokemonTypes = false;
-      this.listPokemonTypes.splice(this.listPokemonTypes.indexOf(item), 1);
-      // item = false;
-      // alert("Chip close clicked" + item);
+      this.filterPokemonTypes.splice(this.filterPokemonTypes.indexOf(item), 1);
     },
-
-    deleteChip(itemNeedToRemove, array) {
-      for (let i = 0; i < array.length; i += 1) {
-        if (array[parseInt(i, 10)] === itemNeedToRemove) {
-          array.splice(i, 1);
-        }
+    getPokemonTypes() {
+      const total = []
+      if(this.allInfo.length > 0) {
+        this.allInfo.forEach(x => {
+          x.types.forEach(y => {
+            total.push(y.type.name)
+          })
+        })
       }
-    },
-    // filteredPokemons() {
-    //   console.log(this.searchValue);
-    //   // var tempInfo = this.allInfo;
-    //   // Process search input
-    //   // if (this.searchValue != "" && this.searchValue) {
-    //   //   console.log("Here");
-    //   //   tempInfo = tempInfo.filter((item) => {
-    //   //     // console.log(item.name);
-    //   //     return (this.pokemons.push = item.name
-    //   //       .toUpperCase()
-    //   //       .includes(this.searchValue.toUpperCase()));
-    //   //   });
-    //   // }
-    //   this.allPokemons = this.allInfo.filter((each) => {
-    //     console.log(each.name.toUpperCase());
-    //     console.log(this.searchValue.toUpperCase());
-    //     return each.name.toUpperCase().includes(this.searchValue.toUpperCase());
-    //   });
-    //   // console.log(filtered);
-    //   // let filtered = this.pokemons.filter((each) => {
-    //   //   console.log(each.toUpperCase());
-    //   //   console.log(this.searchValue.toUpperCase());
-    //   //   return each.toUpperCase().includes(this.searchValue.toUpperCase());
-    //   // });
-    //   // console.log(filtered);
-    // },
-    // setName() {},
-    // setImg() {},
+      this.listPokemonTypes = [...new Set(total)];
+    }
   },
 };
 </script>
